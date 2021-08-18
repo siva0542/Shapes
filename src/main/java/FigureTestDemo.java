b81a6cd2f7dea69ce2e6e0f738ab583d9bcb240f
siva0542 <sivabhargavakanduru@gmail.com>
2021-08-18 21:20:54 +0530

    Requirement-I:Calculating the area of a rectangle

--- src/main/java/FigureTestDemo.java
+++ src/main/java/FigureTestDemo.java
@@ -1,2 +1,15 @@
 public class FigureTestDemo {
+    int length;
+    int breadth;
+    public FigureTestDemo(int length, int breadth) {
+        this.length=length;
+        this.breadth=breadth;
+    }
+
+    public int area() {
+        if(length!=breadth)
+            return length*breadth;
+        else
+            return length*length;
+    }
 }
--- src/test/java/FigureTest.java
+++ src/test/java/FigureTest.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FigureTest {
+
+    @Test
+    public void toCalculateAreaOfRectangleWhenBothDimensionsArePositive(){
+        FigureTestDemo obj=new FigureTestDemo(4,5);
+        int actualArea=obj.area();
+        int expectedArea=20;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    public void toCalculateAreaOfRectangleWhenBothDimensionsAreNegative(){
+        FigureTestDemo obj=new FigureTestDemo(-4,-5);
+        int actualArea=obj.area();
+        int expectedArea=20;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    public void toCalculateAreaOfRectangleWhenLengthIsPositiveAndBreadthIsNegative(){
+        FigureTestDemo obj=new FigureTestDemo(4,-5);
+        int actualArea=obj.area();
+        int expectedArea=-20;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    public void toCalculateAreaOfRectangleWhenLengthIsNegativeAndBreadthIsPositive(){
+        FigureTestDemo obj=new FigureTestDemo(-4,5);
+        int actualArea=obj.area();
+        int expectedArea=-20;
+        assertEquals(actualArea,expectedArea);
+    }
 }
